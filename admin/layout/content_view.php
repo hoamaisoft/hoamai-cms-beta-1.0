2afe3a1645860258a10d8fc892cf2a26bb243630
hoamaisoft <hoamaisoft@gmail.com>
2015-11-15 13:13:17 +0700

    fix view content admincp folder_path

--- admin/layout/content_view.php
+++ admin/layout/content_view.php
@@ -1,5 +1,1 @@
-<?php
-$slug = $args['content']->slug;
-$content_link = SITE_URL.'/'.$slug;
-?>
-<iframe width="100%" src="<?php echo $content_link; ?>" scrolling="no" id="fullheight_iframe" onLoad="calcHeight();" height="1px" frameborder="0" ></iframe>
+<?php$slug = $args['content']->slug;$content_link = SITE_URL.FOLDER_PATH.$slug;?><iframe width="100%" src="<?php echo $content_link; ?>" scrolling="no" id="fullheight_iframe" onLoad="calcHeight();" height="1px" frameborder="0" ></iframe>
